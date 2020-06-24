7057ab57d058e534fa6c0bb06f1d6036ccfffeb8
Harsh Gupta <harshgupta951114@gmail.com>
2020-06-24 18:16:53 +0530

    Updates

---
+++ precache-manifest.5a600f2ce0971d2c636cb104e21fc652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d7e97b12fff4ab5a87b13cb7e27a88",
+    "url": "/mern-app-material-ui/index.html"
+  },
+  {
+    "revision": "08883c7e6eb754599a5b",
+    "url": "/mern-app-material-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "030ec3ecf30627394404",
+    "url": "/mern-app-material-ui/static/js/2.d3f33056.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mern-app-material-ui/static/js/2.d3f33056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08883c7e6eb754599a5b",
+    "url": "/mern-app-material-ui/static/js/main.b70a55c5.chunk.js"
+  },
+  {
+    "revision": "201bfd030a595d206bd4",
+    "url": "/mern-app-material-ui/static/js/runtime-main.81b97c6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mern-app-material-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mern-app/precache-manifest.7da9c4f6d10c9502447b2288001f8258.js"
+  "/mern-app-material-ui/precache-manifest.5a600f2ce0971d2c636cb104e21fc652.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mern-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mern-app-material-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
