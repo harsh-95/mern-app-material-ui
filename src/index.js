fee21f3c4e6e7b891deca005ba74f8e8a1c3f034
Harsh Gupta <harshgupta951114@gmail.com>
2020-06-24 18:45:56 +0530

    added routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MenuBar from './components/AppBar/AppBar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MenuBar />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
